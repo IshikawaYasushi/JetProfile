634f62b6dfd44b76592e8894fd67fe4136edfe4f
ishikawa <iyasu125ever@gamil.com>
2023-08-13 23:25:59 +0900

    ステータスバーの色を変更

--- app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetprofile/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -34,7 +36,10 @@ fun JetProfileTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
     } else {
         LightColorPalette
     }
-
+    rememberSystemUiController().setSystemBarsColor(
+        color = Color.White,
+        darkIcons = true
+    )
     MaterialTheme(
         colors = colors,
         typography = Typography,
